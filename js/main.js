1d060c2430057765025f1b9196772f13dab92d5a
Suldrun45 <suldrun@gmail.com>
2026-05-28 22:41:23 +0200

    Update main.js for animist

--- js/main.js
+++ js/main.js
@@ -1,4 +1,71 @@
 const AVATAR_UUID='Compendium.pf2e.spells-srd.Item.ckUOoqOM7Kg7VqxB';
+const ANIMIST_UUID='Compendium.pf2e.classes.Item.9KiqZVG9r5g8mC4V';
+const AVATAR_APPARITION_UUID='Compendium.pf2e-avatars.avatars.Item.1VPvzF87B6OIvoTN';
+const APPARITION_AVATARS=[{
+    "label": "Crafter in the Vault",
+    "value": "Compendium.pf2e-avatars.avatars.Item.PFGW69TY36yo5inb",
+    "vesselSpell": "Compendium.pf2e.spells-srd.Item.rzcXUF5YmJYxgxEa"
+  },
+  {
+    "label": "Custodian of Groves and Gardens",
+    "value": "Compendium.pf2e-avatars.avatars.Item.7LrXVbYDHGbroiO4",
+    "vesselSpell": "Compendium.pf2e.spells-srd.Item.w4M6Vqvq8k6MOFvj"
+  },
+  {
+    "label": "Echo of Lost Moments",
+    "value": "Compendium.pf2e-avatars.avatars.Item.xe65VimknQXFjnAa",
+    "vesselSpell": "Compendium.pf2e.spells-srd.Item.FZMVLL7HvasjiM6D"
+  },
+  {
+    "label": "Impostor in Hidden Places",
+    "value": "Compendium.pf2e-avatars.avatars.Item.EQ9mYN7JSlUj5h2T",
+    "vesselSpell": "Compendium.pf2e.spells-srd.Item.t1e3U2eluRsp2izf"
+  },
+  {
+    "label": "Lurker in Devouring Dark",
+    "value": "Compendium.pf2e-avatars.avatars.Item.ukEKOzobGGBKS9Nn",
+    "vesselSpell": "Compendium.pf2e.spells-srd.Item.wTldMJx0vyBAehrI"
+  },
+  {
+    "label": "Monarch of the Fey Courts",
+    "value": "Compendium.pf2e-avatars.avatars.Item.cVFRqkoPzo6BDofl",
+    "vesselSpell": "Compendium.pf2e.spells-srd.Item.SAmyaiVKTDrUNjot"
+  },
+  {
+    "label": "Reveler in Lost Glee",
+    "value": "Compendium.pf2e-avatars.avatars.Item.gucr39MJ2SOaAEIK",
+    "vesselSpell": "Compendium.pf2e.spells-srd.Item.cuKVbWWNzJj1GDpZ"
+  },
+  {
+    "label": "Shepherd of Errant Winds",
+    "value": "Compendium.pf2e-avatars.avatars.Item.oMbS3Dc18cp9FXSA",
+    "vesselSpell": "Compendium.pf2e.spells-srd.Item.x2Gf3lt64eoMocMd"
+  },
+  {
+    "label": "Speaker of Sibilance",
+    "value": "Compendium.pf2e-avatars.avatars.Item.MLHZkbkcjCZZsePd",
+    "vesselSpell": "Compendium.pf2e.spells-srd.Item.h2DLv8TQV0Z83tQp"
+  },
+  {
+    "label": "Stalker in Darkened Boughs",
+    "value": "Compendium.pf2e-avatars.avatars.Item.gzw5qFck8lcebord",
+    "vesselSpell": "Compendium.pf2e.spells-srd.Item.BhJtCTLbngvZm8EA"
+  },
+  {
+    "label": "Steward of Stone and Fire",
+    "value": "Compendium.pf2e-avatars.avatars.Item.BJsH049PhBrxjctn",
+    "vesselSpell": "Compendium.pf2e.spells-srd.Item.PrlR2sLWeiuTcPF2"
+  },
+  {
+    "label": "Vanguard of Roaring Waters",
+    "value": "Compendium.pf2e-avatars.avatars.Item.OhbK4vN5zMSU6Dhs",
+    "vesselSpell": "Compendium.pf2e.spells-srd.Item.ew4ToaTU3o8ahKio"
+  },
+  {
+    "label": "Witness to Ancient Battles",
+    "value": "Compendium.pf2e-avatars.avatars.Item.GNZ1THJ5qqoSOGQ7",
+    "vesselSpell": "Compendium.pf2e.spells-srd.Item.X4On99Nti8gjWywG"
+  }];
 
 Hooks.once("ready", async function () {
     Hooks.on("createItem", async (item, data, userId) => {
@@ -26,16 +93,43 @@ Hooks.once("ready", async function () {
 
         if (!itemUuid) return;
         if (itemUuid !== AVATAR_UUID) return;
+        
         const spellcaster = ChatMessage.getSpeakerActor(chatMessage.speaker);
-        const deityUUID = spellcaster?.deity?.sourceId;
-        if (!deityUUID) return;
-        const deity = await fromUuid(deityUUID);
-        if (!deity) return;
-        const deityName = deity.name;
-        const deities = game.packs.find(p => p.collection=='pf2e-avatars.avatars');
-        const avatarEffectEntry = deities.index.find(d => d.name.toLowerCase().includes('avatar of ' + deityName.toLowerCase()));
-        if (!avatarEffectEntry) return;
-        const avatarEffect = await fromUuid(avatarEffectEntry.uuid);
-        await spellcaster.createEmbeddedDocuments("Item", [avatarEffect]);
+        if (!spellcaster) return;
+
+        if (spellcaster.class.sourceId==ANIMIST_UUID) {
+            const avatarEffect = (await fromUuid(AVATAR_APPARITION_UUID)).toObject();
+            if (game.modules.find(p => p.id === 'pf2e-dailies')?.active && !spellcaster.getFlag('pf2e-dailies', 'disabled.dailies.animist') && spellcaster.getFlag('pf2e-dailies', 'extra.dailies.animist.primaryVessels')?.length > 0) {
+                const choices = [];
+                const primaryVessels = spellcaster.getFlag('pf2e-dailies', 'extra.dailies.animist.primaryVessels');
+                primaryVessels.forEach(vessel => {
+                    const vesselId = spellcaster.itemTypes.spell.find(s => s.id==vessel)?.sourceId;
+                    if (vesselId) {
+                        const apparition = APPARITION_AVATARS.find(p=>p.vesselSpell==vesselId);
+                        if (apparition){
+                            choices.push({"label": apparition.label, "value": apparition.value});
+                        }
+                    }
+                });
+                if (choices.length>0){
+                    const rule = {"key":"ChoiceSet","flag":"animistAvatar","choices": choices};
+                    
+                    avatarEffect.system.rules[0] = rule;
+                }
+            }
+            await spellcaster.createEmbeddedDocuments("Item", [avatarEffect]);
+        }
+        else {
+            const deityUUID = spellcaster?.deity?.sourceId;
+            if (!deityUUID) return;
+            const deity = await fromUuid(deityUUID);
+            if (!deity) return;
+            const deityName = deity.name;
+            const deities = game.packs.find(p => p.collection=='pf2e-avatars.avatars');
+            const avatarEffectEntry = deities.index.find(d => d.name.toLowerCase().includes('avatar of ' + deityName.toLowerCase()));
+            if (!avatarEffectEntry) return;
+            const avatarEffect = await fromUuid(avatarEffectEntry.uuid);
+            await spellcaster.createEmbeddedDocuments("Item", [avatarEffect]);
+        }
     });
 });
